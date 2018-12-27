dfd97f5491271afe92556fc1137224a875702fed
hai-nguyen1112 <hainguyen871112@gmail.com>
2018-12-27 15:00:12 -0500

    My the bachelor to do commit

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,69 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each { |seasons, seasons_data|
+    if season == seasons
+      i = 0
+      while i < seasons_data.size
+        if seasons_data[i]["status"] == "Winner"
+          return seasons_data[i]["name"].split(" ")[0]
+        end
+        i += 1
+      end
+    end
+  }
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each { |season, season_data|
+    i = 0
+    while i < season_data.length
+      if season_data[i]["occupation"] == occupation
+        return season_data[i]["name"]
+      end
+    i += 1
+    end
+  }
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each { |season, season_data|
+    i = 0
+    while i < season_data.length
+      if season_data[i]["hometown"] == hometown
+        count += 1
+      end
+      i += 1
+    end
+  }
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each { |season, season_data|
+    i = 0
+    while i < season_data.length
+      if season_data[i]["hometown"] == hometown
+        return season_data[i]["occupation"]
+      end
+      i += 1
+    end
+  }
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  data.each { |seasons, seasons_data|
+    if season == seasons
+      i = 0
+      total_age = 0
+      while i < seasons_data.size
+        if total_age == 0
+        total_age = seasons_data[i]["age"].to_i
+        else
+        total_age = total_age + seasons_data[i]["age"].to_i
+        end
+        i += 1
+      end
+      return (total_age.to_f / seasons_data.length).round
+    end
+  }
 end
